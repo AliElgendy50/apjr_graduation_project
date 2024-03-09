9915f7d64425dd27752829069f2cf7cdc50f6dc2
Abdelrhman-73 <abdelrhman.amgad@gmail.com>
2024-03-09 03:01:06 +0200

    Tweekin of nav params (moi,saif), Added apjr_control package and created wheels_control.py

---
+++ apjr_control/scripts/wheels_control.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import rospy
+from nav_msgs.msg import Odometry
+import math
+ 
+def euler_from_quaternion(x, y, z, w):
+        """
+        Convert a quaternion into euler angles (roll, pitch, yaw)
+        roll is rotation around x in radians (counterclockwise)
+        pitch is rotation around y in radians (counterclockwise)
+        yaw is rotation around z in radians (counterclockwise)
+        """
+        t0 = +2.0 * (w * x + y * z)
+        t1 = +1.0 - 2.0 * (x * x + y * y)
+        roll_x = math.atan2(t0, t1)
+     
+        t2 = +2.0 * (w * y - z * x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        pitch_y = math.asin(t2)
+     
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (y * y + z * z)
+        yaw_z = math.atan2(t3, t4)
+     
+        return roll_x, pitch_y, yaw_z # in radians
+def callback(msg):
+    posX = msg.pose.pose.position.x
+    posY = msg.pose.pose.position.y
+    orinetationX = msg.pose.pose.orientation.x
+    orinetationY = msg.pose.pose.orientation.y    
+    orinetationZ = msg.pose.pose.orientation.z
+    orinetationW = msg.pose.pose.orientation.w
+
+    roll,pitch,yaw = euler_from_quaternion(orinetationX,orinetationY,orinetationZ,orinetationW)
+    print("roll = ",roll)
+    print("pitch = ",pitch)
+    print('yaw = ',yaw)
+
+if __name__=="__main__":
+    rospy.init_node('read_pose')
+    odom_sub = rospy.Subscriber('/odom',Odometry,callback)
+    rospy.spin()
